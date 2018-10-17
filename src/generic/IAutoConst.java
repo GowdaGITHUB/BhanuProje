43f704c368a7ce55477decc0cff4fe277f389b94
Varun Gowda <Varun Gowda@VarunGowda-PC>
2018-10-17 19:07:00 +0530

    BhanuPro

--- src/generic/IAutoConst.java
+++ src/generic/IAutoConst.java
@@ -2,8 +2,10 @@ package generic;
 
 public interface IAutoConst {
 	String CHROME_KEY="webdriver.chrome.driver";
-	String CHROME_VALUE="./driver/chromedriver.exe";
+	String CHROME_VALUE="./Drivers/chromedriver.exe";
 	String GECKO_KEY="webdriver.gecko.driver";
-	String GECKO_VALUE="./driver/geckodriver.exe";
+	String GECKO_VALUE="./Drivers/geckodriver.exe";
+	String RES_PATH="./Result/Summary.xlsx";
 	String CONFIG_PATH="./input/config.properties";
+	String DATA_PATH="./input/data.xlsx";
 }
