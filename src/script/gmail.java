dc1913def61704197f4e3f75947eccb649a2cc5b
91938 <91938@LAPTOP-MAS4RSVA>
2023-02-17 17:44:33 +0530

    2nd commit

---
+++ src/script/gmail.java
@@ -0,0 +1,14 @@
+package script;
+
+public class gmail {
+
+	public gmail() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
